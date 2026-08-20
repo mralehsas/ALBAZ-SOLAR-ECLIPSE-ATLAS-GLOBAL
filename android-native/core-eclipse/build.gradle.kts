a30f147d026372f502a08fb246869b669bdffc97
Omar.Albaz <mralehsas@gmail.com>
2026-08-20 08:54:24 +0300

    feat: import audited Besselian eclipse catalog

--- android-native/core-eclipse/build.gradle.kts
+++ android-native/core-eclipse/build.gradle.kts
@@ -6,9 +6,19 @@ kotlin {
     jvmToolchain(17)
 }
 
+sourceSets {
+    main {
+        resources {
+            srcDir(rootProject.projectDir.parentFile)
+            include("besselian_data.js")
+        }
+    }
+}
+
 dependencies {
     implementation(project(":core-domain"))
     testImplementation(libs.junit.jupiter)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
